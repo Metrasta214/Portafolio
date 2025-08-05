559ea3ab31e85ccc65282aea2c6a4077105a9f20
Alvaro Madrid <metrasta211@gmail.com>
2025-08-05 14:13:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Portafolio cargado correctamente.");
+
+// Menú hamburguesa
+        document.querySelector('.menu-toggle').addEventListener('click', () => {
+            document.querySelector('.nav-links').classList.toggle('active');
+        });
+
+        // Desplazamiento suave
+        document.querySelectorAll('.nav-links a').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
